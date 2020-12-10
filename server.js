b84111fa043d7ba91917d58f2dce44cdaa39e245
Geoff Ball <geoff@gball.design>
2020-12-10 09:58:38 -0700

    Restrict access to my domain

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
+  // res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
