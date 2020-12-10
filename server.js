bf4d15f885e657ab9ed212cbf69400bf1b68c476
Geoff Ball <geoff@gball.design>
2020-12-10 11:10:36 -0700

    Remove pointless comment

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 3001;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
-  // res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
